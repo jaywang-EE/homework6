d2d5ea05a3c5142aca00bf05d89553e36e2970ad
jaywangUmich <70972293+jaywangUmich@users.noreply.github.com>
2020-11-12 19:35:21 -0500

    Initial commit

---
+++ js/video.js
@@ -0,0 +1,59 @@
+var video;
+
+window.addEventListener("load", function() {
+	video = document.querySelector("#myVideo");
+	document.querySelector("#volume").innerHTML = "100%";
+});
+
+document.querySelector("#play").addEventListener("click", function() {
+	console.log("Play Video");
+	video.play(); 
+});
+
+document.querySelector("#pause").addEventListener("click", function() {
+	myVideo.pause(); 
+});
+
+document.querySelector("#slower").addEventListener("click", function() {
+	video.playbackRate -= 0.1;
+});
+
+document.querySelector("#faster").addEventListener("click", function() {
+	video.playbackRate += 0.1;
+});
+
+document.querySelector("#skip").addEventListener("click", function() {
+	video.currentTime += 5; 
+	if (video.currentTime >= video.duration) {
+		video.currentTime = 0;
+		video.play(); 
+	}
+	console.log(video.currentTime + ", " + video.duration);
+});
+
+document.querySelector("#mute").addEventListener("click", function() {
+	if (myVideo.muted) {
+		myVideo.muted = false;
+		document.querySelector("#mute").innerHTML = "Mute";
+	} else {
+		myVideo.muted = true;
+		document.querySelector("#mute").innerHTML = "Unmute";
+	}
+});
+
+document.querySelector("#old").addEventListener("click", function() {
+	video.classList.add("oldTime");
+});
+
+document.querySelector("#original").addEventListener("click", function() {
+	video.classList.remove("oldTime");
+});
+
+
+function onVolumeChange () {
+	var volumeSlider = document.querySelector("#volumeSlider");
+	video.volume = volumeSlider.value/100.0;
+	console.log(volumeSlider.value);
+	console.log(video.volume);
+	document.querySelector("#volume").innerHTML = volumeSlider.value + "%";
+}
